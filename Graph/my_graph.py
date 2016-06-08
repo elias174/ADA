6d3dd17713fe7a948fbbd0ad48f10b9b58d889a4
elias174 <elias.valencia@ucsp.edu.pe>
2016-06-08 15:57:09 -0500

    Added Graph Structure

---
+++ Graph/my_graph.py
@@ -0,0 +1,42 @@
+from collections import defaultdict
+
+
+class MyGraph(object):
+
+    def __init__(self, connections, directed=False):
+        self._graph = defaultdict(set)
+        self._directed = directed
+        self.add_connections(connections)
+
+    def add_connections(self, connections):
+        for node1, node2 in connections:
+            self.add(node1, node2)
+
+    def add(self, node1, node2):
+        self._graph[node1].add(node2)
+        if not self._directed:
+            self._graph[node2].add(node1)
+
+    def remove(self, node):
+        for n, cxns in self._graph.iteritems():
+            try:
+                cxns.remove(node)
+            except KeyError:
+                pass
+        try:
+            del self._graph[node]
+        except KeyError:
+            pass
+
+    def is_connected(self, node1, node2):
+
+        return node1 in self._graph and node2 in self._graph[node1]
+
+    def __str__(self):
+        return '{}({})'.format(self.__class__.__name__, dict(self._graph))
+
+connections = [('A', 'B'), ('B', 'C'), ('B', 'D'),
+             ('C', 'D'), ('E', 'F'), ('F', 'C')]
+
+g = MyGraph(connections)
+print g
